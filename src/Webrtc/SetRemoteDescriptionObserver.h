19961b17937eb7f954aa6ed6687e688d2b4983fb
joyflix99 <joyflix99@gmail.com>
2023-03-02 12:18:39 +0530

    SetRemoteDescriptionObserver.cpp

---
+++ src/Webrtc/SetRemoteDescriptionObserver.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <api/jsep.h>
+#include <api/set_remote_description_observer_interface.h>
+
+#include "WebRTCPlugin.h"
+
+namespace webrtc
+{
+class SetRemoteDescriptionObserver;
+using DelegateSetRemoteDesc = void (*)(PeerConnectionObject*, SetRemoteDescriptionObserver*, RTCErrorType, const char*);
+
+class SetRemoteDescriptionObserver : public ::webrtc::SetRemoteDescriptionObserverInterface
+{
+public:
+    static rtc::scoped_refptr<SetRemoteDescriptionObserver> Create(PeerConnectionObject* connection);
+    static void RegisterCallback(DelegateSetRemoteDesc callback) { s_setRemoteDescCallback = callback; }
+
+    void OnSetRemoteDescriptionComplete(RTCError error) override;
+
+protected:
+    explicit SetRemoteDescriptionObserver(PeerConnectionObject* connection);
+    ~SetRemoteDescriptionObserver() override = default;
+
+private:
+    PeerConnectionObject* m_connection;
+    static DelegateSetRemoteDesc s_setRemoteDescCallback;
+};
+
+}
